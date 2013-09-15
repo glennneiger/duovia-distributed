c020c3eb356c1668777c52178ad0e8ee0f8200c8
Tyler Jensen <tyler.jensen@gmail.com>
2013-09-15 11:26:40 -0600

    doc wip

--- src/Tests/Integration/DuoVia.Net.DistributedConsoleTest/Program.cs
+++ src/Tests/Integration/DuoVia.Net.DistributedConsoleTest/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using DuoVia.Net.Distributed;
-using DuoVia.Net.Distributed.Server;
 
 namespace DuoVia.Net.DistributedConsoleTest
 {
